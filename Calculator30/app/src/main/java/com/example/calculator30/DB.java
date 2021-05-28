418a4c8e02fb788fb93bb0f0423734ce7da2acb0
VladWolfsbane <vladwolfsbane.vw@gmail.com>
2021-05-28 22:46:53 +0300

    upload

---
+++ Calculator30/app/src/main/java/com/example/calculator30/DB.java
@@ -0,0 +1,19 @@
+package com.example.calculator30;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DB {
+
+    private DatabaseReference databaseReference;
+
+    public DB(){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        databaseReference = db.getReference(Data.class.getSimpleName());
+    }
+
+    public Task<Void> add(Data data){
+        return databaseReference.push().setValue(data);
+    }
+}
